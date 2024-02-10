0fee719247ffc9130396e22744c7b8bce551da97
James Hawk <jph94880@gmail.com>
2024-02-10 16:18:26 -0600

    Makes basic css imporvements

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,18 +4,18 @@ const API = ''
 
 const Card = ({ clickHandler }) => {
 
-    useEffect(() => {
-        fetch(API)
-        .then((r) => r.json())
-        .then(setCount)
-    })
+    // useEffect(() => {
+    //     fetch(API)
+    //     .then((r) => r.json())
+    //     .then(setCount)
+    // })
 
     const [count, setCount] = useState()
   return (
     <section className="card">
-        <button onClick={clickHandler}></button>
-        <h1>Test</h1>
-        <p>{count}</p>
+        <h2>Test</h2>
+        <button onClick={clickHandler}>Click me!</button>
+        <p>I'm a card!</p>
 
     </section>
   )
